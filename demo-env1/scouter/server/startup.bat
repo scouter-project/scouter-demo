c4dc31cfd2c3f387ffdfe10a48e262bf2ad07e73
Gunhee Lee <gunlee01@gmail.com>
2017-07-02 15:40:43 +0900

    version up for 1.7.1

--- demo-env1/scouter/server/startup.bat
+++ demo-env1/scouter/server/startup.bat
@@ -2,7 +2,7 @@
 setlocal
 set originDir=%cd%
 cd /D %~dp0
-java -Xmx512m -classpath ./boot.jar scouter.boot.Boot ./lib
+java -Xmx512m -classpath ./scouter-server-boot.jar scouter.boot.Boot ./lib
 cd /D %originDir%
 echo "Scouter Server Stopped..."
 pause
